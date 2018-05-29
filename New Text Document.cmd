cb3046e591ea0a51f3c2f314f077a310bf144f47
Ken Leung <github@iken.be>
2018-05-29 14:44:08 -0700

    removed cmd

--- New Text Document.cmd
+++
@@ -1,7 +0,0 @@
-git checkout --orphan newBranch
-git add -A  # Add all files and commit them
-git commit -m ok
-git branch -D master  # Deletes the master branch
-git branch -m master  # Rename the current branch to master
-git push -f origin master  # Force push master branch to github
-git gc --aggressive --prune=all     # remove the old files
